6cf14abda93a093dfa606a389257acb56b6625a0
Ivonne Martinez <inmartinezt@gmail.com>
2024-12-05 20:42:41 -0500

    Interaction with notes
    Player can interact with radio and notes inside the Level 3.
    Canva is activated with triggers.

---
+++ Assets/Scripts/Level3/Objectinteraction.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Manages the interaction with the 3 letters into the Level 3 and those appears on Canvas.
+/// Author: Ivonne Martinez
+/// Date: 21/11/2024
+/// </summary>
+public class ObjectInteraction : MonoBehaviour
+{
+    public Image note1Image; // Image for Note1
+    public Image note2Image; // Image for Note2
+    public Image morseImage; // Image for Morse
+
+    private bool isNear = false; // If player is near the object
+    private string currentObjectTag = ""; // To track which object we are interacting with
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object is tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            isNear = true;
+            Debug.Log("Player is near: " + gameObject.name);
+            currentObjectTag = gameObject.tag; // Set the current object tag
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the object is tagged as "Player"
+        if (other.CompareTag("Player"))
+        {
+            isNear = false;
+            Debug.Log("Player left: " + gameObject.name);
+            currentObjectTag = ""; // Reset tag when player leaves
+        }
+    }
+
+    private void Update()
+    {
+        if (isNear && Input.GetKeyDown(KeyCode.E)) // If the player is near and presses "E"
+        {
+            // Toggle the Canvas images based on the tag of the object
+            switch (currentObjectTag)
+            {
+                case "Note1":
+                    note1Image.gameObject.SetActive(!note1Image.gameObject.activeSelf); // Toggle visibility
+                    break;
+                case "Note2":
+                    note2Image.gameObject.SetActive(!note2Image.gameObject.activeSelf); // Toggle visibility
+                    break;
+                case "Morse":
+                    morseImage.gameObject.SetActive(!morseImage.gameObject.activeSelf); // Toggle visibility
+                    break;
+            }
+        }
+    }
+}
