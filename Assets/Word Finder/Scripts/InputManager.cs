7d46db2500cf86ce761f3746715ece999428e310
Pedro Barrios <p.argel03@gmail.com>
2024-12-10 15:25:14 -0500

    Correcciones

--- Assets/Word Finder/Scripts/InputManager.cs
+++ Assets/Word Finder/Scripts/InputManager.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour
 {
     [Header(" Elements ")]
     [SerializeField] private WordContainer[] wordContainers;
-
+    public GameObject error;
+    public GameObject correct;
+    public GameObject notificacion;
 
     [Header(" Settings ")]
     private int currentWordContainerIndex;
     // Start is called before the first frame update
     void Start()
     {
+
         Initialize();
 
         KeyboardKey.onKeyPressed += KeyPressedCallback;
+        error.SetActive(false);
+        correct.SetActive(false);
+        notificacion.SetActive(false);
     }
 
     // Update is called once per frame
@@ -50,12 +57,33 @@ public class InputManager : MonoBehaviour
          string secretWord = WordManager.instance.GetSecretWord();
 
          if(wordToCheck == secretWord)
+         {
             Debug.Log("Correct word");
+            StartCoroutine(Correct());
+         }
          else
          {
               Debug.Log("Wrong word");
               currentWordContainerIndex++;
+            StartCoroutine(Error());
          }
 
+    }
+
+    IEnumerator Error()
+    {
+        error.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        error.SetActive(false);
+    }
+
+    IEnumerator Correct()
+    {
+        correct.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        correct.SetActive(false);
+        notificacion.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("Level3");
     }
 }
